f6c5a548c4365bd645f7c88a58785d1ac50094dc
Andrew Le <s3820098@rmit.edu.vn>
2022-08-10 17:16:57 +0700

    11. selection for animals and breeds

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
+const ANIMALS = ["dogs", "cats", "bears", "lions", "penguins"];
+const BREEDS = [];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+
   return (
     <div className="search-params">
       <form>
@@ -13,6 +19,44 @@ const SearchParams = () => {
             value={location}
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option value=""></option>
+            {ANIMALS.map((animal) => {
+              return (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option value=""></option>
+            {BREEDS.map((breed) => {
+              return (
+                <option key={breed} value={breed}>
+                  {breed}
+                </option>
+              );
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>
